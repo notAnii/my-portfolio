c3f643e0eda17a916e477e2c1a4f03737eadf90d
notAnii <animazingtoho@gmail.com>
2025-05-02 13:50:52 +0400

    fix: add experience to nav bar

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -34,7 +34,7 @@ const Experience = async () => {
   };
 
   return (
-    <div className="py-20" id="testimonials">
+    <div className="py-20" id="experience">
       <h1 className="heading">
         My <span className="text-purple">work experience</span>
       </h1>
